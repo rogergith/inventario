6a9f7c9bb9a25a5459d02d88340a5b729da0c37f
Roger <raismaelr@gmail.com>
2017-09-20 07:29:29 -0400

    Creacion de la entidad Vendor

--- src/main/java/com/inventario/entity/Vendor.java
+++ src/main/java/com/inventario/entity/Vendor.java
@@ -1,10 +1,74 @@
 package com.inventario.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "VENDOR")
 public class Vendor {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "VENDORID")
+	private int vendorid;
+	
+	@Column(name = "ADDRESS")
+	private String address;
+	
+	@Column(name= " CONTACT")
+	private String contact;
+	
+	@Column(name = "VENDORNAME")
+	private String vendorname;
+	
+	@Column(name = "PHONE")
+	private String phone;
+	
+	public Vendor() {}
+
+	public int getVendorid() {
+		return vendorid;
+	}
+
+	public void setVendorid(int vendorid) {
+		this.vendorid = vendorid;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getVendorname() {
+		return vendorname;
+	}
+
+	public void setVendorname(String vendorname) {
+		this.vendorname = vendorname;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
 }
